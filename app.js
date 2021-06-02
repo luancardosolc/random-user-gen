790dcc39f927d074ff572a9977b3b22ff084c56f
Luan Cardoso <luanleitecardoso@gmail.com>
2021-06-02 11:45:55 -0300

    Passing some values to the html

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const app = Vue.createApp({
-  template: '<h1>Hello World</h1>'
+  data() {
+    return {
+      firstName: 'Luan',
+      lastName: 'Cardoso',
+      email: 'luan@luan.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/10.jpg'
+    }
+  }
 });
 
 app.mount('#app');
